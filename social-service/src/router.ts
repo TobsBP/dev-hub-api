73f03844e47d98001445bc3363357f30143c76b5
tobsBP <tobias.bp2004@gmail.com>
2026-04-01 22:43:33 -0300

    feat(social-service): register all routes with RESTful naming and Swagger    tags

---
+++ social-service/src/router.ts
@@ -0,0 +1,24 @@
+import type { FastifyInstance } from 'fastify';
+import { bookmarkRoutes } from '@/routes/bookmark.js';
+import { codeSnippetRoutes } from '@/routes/codeSnippet.js';
+import { commentRoutes } from '@/routes/comment.js';
+import { followRoutes } from '@/routes/follow.js';
+import { likeRoutes } from '@/routes/like.js';
+import { postRoutes } from '@/routes/post.js';
+import { postSolutionRoutes } from '@/routes/postSolution.js';
+import { reputationRoutes } from '@/routes/reputation.js';
+import { tagRoutes } from '@/routes/tag.js';
+import { userRoutes } from '@/routes/user.js';
+
+export const routes = async (app: FastifyInstance) => {
+	app.register(postRoutes);
+	app.register(userRoutes);
+	app.register(commentRoutes);
+	app.register(likeRoutes);
+	app.register(followRoutes);
+	app.register(tagRoutes);
+	app.register(codeSnippetRoutes);
+	app.register(bookmarkRoutes);
+	app.register(postSolutionRoutes);
+	app.register(reputationRoutes);
+};
