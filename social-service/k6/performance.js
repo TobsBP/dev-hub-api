b57c6906afa375ebec259dea942f65d9c7e22988
Tobias <tobias.bp2004@gmail.com>
2026-04-07 09:58:45 -0300

    feat: performance test with pipeline

--- social-service/k6/performance.js
+++ social-service/k6/performance.js
@@ -1,8 +1,8 @@
+import { textSummary } from 'https://jslib.k6.io/k6-summary/0.0.1/index.js';
+import { htmlReport } from 'https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js';
 import { check, group, sleep } from 'k6';
 import http from 'k6/http';
 import { Rate } from 'k6/metrics';
-import { htmlReport } from 'https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js';
-import { textSummary } from 'https://jslib.k6.io/k6-summary/0.0.1/index.js';
 
 const BASE_URL = __ENV.BASE_URL || 'http://localhost:3333';
 
