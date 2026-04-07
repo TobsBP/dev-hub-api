71b5fbd5636494ea3f1c3128c5f7e8aaffeec331
Tobias <tobias.bp2004@gmail.com>
2026-04-07 10:20:02 -0300

    refactor: removed locallhost url

--- social-service/k6/performance.js
+++ social-service/k6/performance.js
@@ -4,7 +4,7 @@ import { check, group, sleep } from 'k6';
 import http from 'k6/http';
 import { Rate } from 'k6/metrics';
 
-const BASE_URL = __ENV.BASE_URL || 'http://localhost:3333';
+const BASE_URL = __ENV.BASE_URL;
 
 const SAMPLE_USER_ID = __ENV.USER_ID;
 const SAMPLE_POST_ID = __ENV.POST_ID;
