541fdc80dc03d5c5c7dea3f34d756fd4b0e60c3a
Tobias <tobias.bp2004@gmail.com>
2026-04-07 10:41:53 -0300

    reafactor: remove http message

--- social-service/src/server.ts
+++ social-service/src/server.ts
@@ -67,6 +67,5 @@ app.setErrorHandler((error: FastifyError, _request, reply) => {
 });
 
 app.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
-	console.log('HTTP server running on http://localhost:3333!');
 	console.log('Docs available at http://localhost:3333/docs');
 });
