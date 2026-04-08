cd176c2dde68fd6e7f31a0448ddbb6ffde54d68e
Tobias <tobias.bp2004@gmail.com>
2026-04-08 14:37:01 -0300

    chore: force test

--- social-service/src/server.ts
+++ social-service/src/server.ts
@@ -28,7 +28,7 @@ app.register(fastifyCors, {
 });
 
 if (!process.env.JWT_SECRET) {
-	throw new Error('JWT_SECRET is not defined in .env');
+	throw new Error('JWT_SECRET is not defined .env');
 }
 
 app.register(fastifyJwt, {
